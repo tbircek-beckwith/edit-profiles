0a515a55eade4bad30e1c1e71f54280e078805c4
tbircek <tbircek@beckwithelectric.com>
2020-04-28 14:49:17 -0400

    updates file name generation algorithm

--- Profiles/Operations/GenerateNewFileName.cs
+++ Profiles/Operations/GenerateNewFileName.cs
@@ -11,6 +11,54 @@ namespace EditProfiles.Operations
     partial class ProcessFiles : IStartProcessInterface
     {
 
+        #region Private Variables
+
+        // holds replacement words with ignoring casing.
+        // must add or remove words here to provide replacements.
+        Dictionary<string, string> FileNameTerms = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                // {"original", "replacement"},
+                {"m-6200a", "M-6200B_"},
+                {"forward power", "Fwd Pwr_"},
+                {"fp", "Fwd Pwr_"},
+                {"reverse power", "Rev Pwr_"},
+                {"rp", "Rev Pwr_"},
+                {"smart reverse power", "AD_" },
+                {"src reverse power", "Src Rev Pwr_" },
+                {"src reverse", "Src Rev Pwr_" },
+                {"src", "Src Rev Pwr_" },
+                {"smart src reverse power", "ADM_" },
+                {"adm", "ADM_" },
+                {"ad", "AD_" },
+                {"dg", "DG_" },
+                {"test", string.Empty},
+                {"short version", string.Empty},
+            };
+
+        // holds replacement words with ignoring casing.
+        // must add or remove words here to provide replacements.
+        Dictionary<string, string> FolderNameTerms = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                // {"original", "replacement"},
+                {"alarm", "alarms"},
+                {"bandcenter", "bandcenters"},
+                {"bandwidth", "bandwidths"},
+                {"definite", "definite-time-delays"},
+                {"distributed", "distributed-generations"},
+                {"georgia", "georgia-power"},
+                {"intertap", "intertap-time-delays"},
+                {"interse", "inverse-time-delays"},
+                {"ldc", "ldc-settings"},
+                {"line limit", "line-limits"},
+                {"pulse width", "output-pulse-widths"},
+                {"varbias", "varbias"},
+                {"smart vr", "voltage-reductions"},
+                {"voltage reduction", "voltage-reductions"},
+                {"correction", "vt-corrections"},
+            };
+
+        #endregion
+
         #region Properties
 
         private StringBuilder NewFileName { get; set; }
@@ -72,13 +120,11 @@ namespace EditProfiles.Operations
             // used http://regexr.com to generate following expressions.
             //
             // combination "Regular Expressions" pattern with group names.
-            // (?<powerScheme>\b(([Ff]orward\s)?|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s))?)([Pp]ower)\b\w*)|(?<product>\b([Mm]-6200[Aa])\b\w*)|(?<revision>\b[Rr]ev(\d)\b\w*)|(?<notUsed>\b([Tt]est*)|([Ss]hort\s[Vv]ersion)\b\w*)|(?<frequency>\b\d{2}[Hh][Zz]\b\w*)|(?<profile>\b[Pp](\d)\b\w*)|(?<determination>\b[Aa][Dd]([Mm])?\b\w*)|(?<distrubuted>\b[Dd][Gg]\b\w*)
+            // (?<powerKeyword>\b(([Ff]orward\s)|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s)))([Pp]ower)\b\w*)|(?<product>\b([Mm]-6200[Aa])\b\w*)|(?<revision>\b[Rr]ev\d\b\w*)|(?<notUsed>\b([Tt]est*)|([Ss]hort\s[Vv]ersion)\b\w*)|(?<frequency>\b\d{2}[Hh][Zz]\b\w*)|(?<profile>\b[Pp](\d)\b\w*)|(?<determination>\b[Aa][Dd]([Mm])?\b\w*)|(?<distrubuted>\b[Dd][Gg]\b\w*)|(?<powerInitials>\b([Ff][Pp]|([Rr][Pp]))\b\w*)|(?<srcReverse>\b([Ss]rc\s)([Rr]everse)\b\w*)|(?<src>\b([Ss]rc)\b\w*)
             //
             // power scheme "Regular Expressions" pattern group.
             // captures (Forward || (Smart || Src || Reverse)) Power like string in the file name.
-            // (?<powerScheme>\b(([Ff]orward\s)?|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s))?)([Pp]ower)\b\w*)
-            // (?<powerScheme>\b([Ff]orward\s)?|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s))([Pp]ower)\b\w*)
-            // (?<powerScheme>\b([Ff]orward\s)?|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s))([Pp]ower)\b\w*)
+            // (?< powerKeyword >\b(([Ff]orward\s) | (([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s)))([Pp]ower)\b\w*)
             //
             // determination "Regular Expressions" pattern group.
             // captures (AD || ADM) like string in the file name.
@@ -104,18 +150,30 @@ namespace EditProfiles.Operations
             // captures P1 like string in the file name.
             // (?<profile>\b[Pp](\d)\b\w*)
             //
+            // powerInitials "Regular Expressions" pattern group.
+            // captures FP or RP like strings in the file name.
+            // (?<powerInitials>\b([Ff][Pp]|([Rr][Pp]))\b\w*)
+            //
+            // srcReverse "Regular Expressions" pattern group.
+            // captures Src Reverse like strings in the file name.
+            // (?<srcReverse>\b([Ss]rc\s)([Rr]everse)\b\w*)
+            //
+            // src "Regular Expressions" pattern group.
+            // captures Src like strings in the file name.
+            // (?<src>\b([Ss]rc)\b\w*)
+            //
             // not used "Regular Expressions" pattern group.
             // captures (Test || (Short Version)) like string in the file name.
             // (?<notUsed>\b([Tt]est*)|([Ss]hort\s[Vv]ersion)\b\w*)
 
             // combination Regex pattern with group names
-            string pattern = @"(?<powerScheme>\b(([Ff]orward\s)|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s)))([Pp]ower)\b\w*)|(?<product>\b([Mm]-6200[Aa])\b\w*)|(?<revision>\b[Rr]ev\d\b\w*)|(?<notUsed>\b([Tt]est*)|([Ss]hort\s[Vv]ersion)\b\w*)|(?<frequency>\b\d{2}[Hh][Zz]\b\w*)|(?<profile>\b[Pp](\d)\b\w*)|(?<determination>\b[Aa][Dd]([Mm])?\b\w*)|(?<distrubuted>\b[Dd][Gg]\b\w*)";
+            string testFileNamePatterns = @"(?<powerKeyword>\b(([Ff]orward\s)|(([Ss]mart\s)?([Ss]rc\s)?([Rr]everse\s)))([Pp]ower)\b\w*)|(?<product>\b([Mm]-6200[Aa])\b\w*)|(?<revision>\b[Rr]ev\d\b\w*)|(?<notUsed>\b([Tt]est*)|([Ss]hort\s[Vv]ersion)\b\w*)|(?<frequency>\b\d{2}[Hh][Zz]\b\w*)|(?<profile>\b[Pp](\d)\b\w*)|(?<determination>\b[Aa][Dd]([Mm])?\b\w*)|(?<distrubuted>\b[Dd][Gg]\b\w*)|(?<powerInitials>\b([Ff][Pp]|([Rr][Pp]))\b\w*)|(?<srcReverse>\b([Ss]rc\s)([Rr]everse)\b\w*)|(?<src>\b([Ss]rc)\b\w*)";
 
             // Regex to capture combination pattern with 100 milliseconds timeout.
-            Regex fileName = new Regex(pattern, RegexOptions.None, TimeSpan.FromMilliseconds(100));
+            Regex fileName = new Regex(testFileNamePatterns, RegexOptions.None, TimeSpan.FromMilliseconds(100));
 
             // Remove every matching pattern to exposed root test name like "Bandwidth"
-            string testName = fileName.Replace(Path.GetFileNameWithoutExtension(FileNameWithPath), string.Empty).Trim() + "_";
+            string testName = fileName.Replace(Path.GetFileNameWithoutExtension(FileNameWithPath), string.Empty).Trim();
 
             string fileNameWithoutRevision = Path.GetFileNameWithoutExtension(FileNameWithPath);
 
@@ -134,18 +192,38 @@ namespace EditProfiles.Operations
             string output = string.Empty;
 
             // stroll through matching patterns to get replacements.
-            foreach (Match match in Regex.Matches(fileNameWithoutRevision, pattern))
+            foreach (Match match in Regex.Matches(fileNameWithoutRevision, testFileNamePatterns))
             {
                 Debug.WriteLine($"Match: {match.Value}");
                 // stitch replacement words together.
-                output += ReplaceFileNameWords(match.Value);
+                output += ReplaceFileNameWords(match.Value, FileNameTerms); // DictionaryOptions.fileNameTerms); // ReplaceFileNameWords_Old(match.Value); // 
             }
 
+            // temp storage to keep replacement words.
+            string testSubFolderName = string.Empty;
+
+            // combination Regex pattern with group names
+            string testFolderNamePatterns = @"(?<alarms>\b((?=Alarm)\b\w*))|(?<bandcenters>\b((?=Bandcenter)\b\w*))|(?<bandwidth>\b((?=Bandwidth)\b\w*))|(?<dtDelay>\b((?=Definite)\b\w*))|(?<dg>\b((?=Distributed)\b\w*))|(?<georgia>\b((?=Georgia)\b\w*))|(?<intertap>\b((?=Inter[Tt]ap)\b\w*))|(?<interse>\b((?=Inverse)\b\w*))|(?<ldc>\b((?=[Ll][Dd][Cc][\s])\b\w*))|(?<limit>\b((?=Line Limit)\b\w*))|(?<pulse>\b((?=Pulse Width)\b\w*))|(?<varbias>\b((?=VAr[Bb]ias)\b\w*))|(?<smartVR>\b(Smart VR)\b\w*)|(?<vr>\b(Voltage Reduction (?!Alarm))\b\w*)|(?<vt>\b(Correction)\b\w*)";
+
+            // stroll through matching patterns to get replacements.
+            foreach (Match match in Regex.Matches(testName, testFolderNamePatterns))
+            {
+                Debug.WriteLine($"Match: {match.Value}");
+                // stitch replacement words together.
+                testSubFolderName += ReplaceFileNameWords(match.Value, FolderNameTerms); // DictionaryOptions.folderNameTerms);
+            }
+            
             // index of first '_' is right after product number
             int productNameLength = output.IndexOf('_') + 1;
+
             // split replacement words to insert test file name and new short name for "Regulator #" in the file name, also append file extension.
             // CurrentRegulatorValue is 0 based.
-            string newFileName = Path.Combine(Path.GetDirectoryName(FileNameWithPath), output.Substring(0, productNameLength) + testName + $"Reg {CurrentRegulatorValue + 1}_" + output.Substring(productNameLength, output.Length - productNameLength))+ Path.GetExtension(FileNameWithPath);
+            string modifiedFolderName = Path.Combine(MyResources.Strings_ModifedFolderName, testSubFolderName);
+            string regulatorFolderName = $"regulator {CurrentRegulatorValue + 1}";
+            string profileFolderName = $"profile {output.Substring(productNameLength + 1, 1)}";
+            string testFolderName = Path.Combine(modifiedFolderName, Path.Combine(regulatorFolderName, new Regex(@"(?<profile>\b[Pp](\d)\b\w*)", RegexOptions.None, TimeSpan.FromMilliseconds(100)).IsMatch(fileNameWithoutRevision) ? profileFolderName : string.Empty)).ToLower();
+            string newFileName = Path.Combine(Path.Combine(Path.GetDirectoryName(FileNameWithPath), testFolderName), $"{output.Substring(0, productNameLength)}{testName}_Reg {CurrentRegulatorValue + 1}_{output.Substring(productNameLength, output.Length - productNameLength)}{Path.GetExtension(FileNameWithPath)}");
+
             return newFileName;
         }
 
@@ -153,26 +231,10 @@ namespace EditProfiles.Operations
         /// Replaces words per the dictionary provided.
         /// </summary>
         /// <param name="wordToReplace">this is the word to look for a replacement in the dictionary</param>
+        /// <param name="searchTerms">select dictionary to use in replacements</param>
         /// <returns>Returns a replacement word if the word provided exists in the dictionary</returns>
-        private string ReplaceFileNameWords(string wordToReplace)
+        private string ReplaceFileNameWords(string wordToReplace, Dictionary<string, string> searchTerms) // DictionaryOptions options)
         {
-            // holds replacement words with ignoring casing.
-            // must add or remove words here to provide replacements.
-            var searchTerms = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-            {
-                // {"original", "replacement"},
-                {"m-6200a", "M-6200B_"},
-                {"forward power", "Fwd Pwr_"},
-                {"reverse power", "Rev Pwr_"},
-                {"smart reverse power", "Smart Rev Pwr_" },
-                {"src reverse power", "Src Rev Pwr_" },
-                {"smart src reverse power", "Smart Src Rev Pwr_" },
-                {"adm", "ADM_" },
-                {"ad", "AD_" },
-                {"dg", "DG_" },
-                {"test", string.Empty},
-                {"short version", string.Empty},
-            };
 
             // holds dictionary entires as a "Regular Expression" search pattern.
             string pattern = "(.)*[^<>]list|" + GetKeyList(searchTerms);
@@ -215,7 +277,7 @@ namespace EditProfiles.Operations
 
             try
             {
-                
+
                 // Generate a new file name.
                 NewFileName.Append(ProcessFileName());
 
@@ -230,23 +292,10 @@ namespace EditProfiles.Operations
                 }
 
                 // Create new folder to store modified files.
-                if (!(Directory.Exists(Path.Combine(
-                                            Path.GetDirectoryName(
-                                             NewFileName.ToString()),
-                                            MyResources.Strings_ModifedFolderName))))
+                if(!Directory.Exists(Path.GetDirectoryName(NewFileName.ToString())))
                 {
-                    Directory.CreateDirectory(Path.Combine(
-                                                Path.GetDirectoryName(
-                                                 NewFileName.ToString()),
-                                                MyResources.Strings_ModifedFolderName));
+                    Directory.CreateDirectory(Path.GetDirectoryName(NewFileName.ToString()));
                 }
-
-                // Replace old folder path with the new location.
-                NewFileName.Replace(Path.GetDirectoryName(
-                                                   NewFileName.ToString()),
-                                                   Path.Combine(Path.GetDirectoryName(
-                                                    NewFileName.ToString()),
-                                                   MyResources.Strings_ModifedFolderName));
 
                 Debug.WriteLine($"New file name is {NewFileName.ToString()}");
 
