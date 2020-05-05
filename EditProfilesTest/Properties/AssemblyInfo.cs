00d432e073e7a5872b18ad15575512e49ab0a907
tbircek <tbircek@beckwithelectric.com>
2020-05-05 13:03:50 -0400

    adds unit testing for the file name generation

---
+++ EditProfilesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EditProfilesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EditProfilesTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("708be493-4a0c-43ab-a807-78eb1ce9572c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EditProfilesTest/TestGenerateNewFileNameFunctions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using EditProfiles.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EditProfilesTest
+{
+    [TestClass]
+    public class TestGenerateNewFileNameFunctions
+    {
+        string FileNameWithPath = $@"\\eng04-win10\c$\Users\tbircek\Desktop\m6200b\drb#186-run-up\ready-for-modbus-mod-test\M-6200B Runup AD 60Hz.occ";
+        string ExpectedTestName = $"Runup";
+
+        string FileNameWithoutRevision = $"M-6200B Runup AD 60Hz";
+        string ExpectedKeywords = $"M-6200B_AD_60Hz";
+
+        string ExpectedSubFolderName = $"Runup";
+
+        [TestMethod]
+        public void CheckTestName()
+        {
+            // Remove every matching pattern to exposed root test name like "Bandwidth"
+            string testName = new AnalyzeValues().Extract(input: Path.GetFileNameWithoutExtension(FileNameWithPath), pattern: new AnalyzeValues().TestFileNamePatterns, keywords: new AnalyzeValues().FileNameKeywords);
+
+            Assert.AreEqual(ExpectedTestName, testName);
+
+            // temp storage to keep replacement words.
+            string keywords = new AnalyzeValues().Replace(input: FileNameWithoutRevision, pattern: new AnalyzeValues().TestFileNamePatterns, keywords: new AnalyzeValues().FileNameKeywords);
+
+            Assert.AreEqual(ExpectedKeywords, keywords);
+            
+            // temp storage to keep replacement words.
+            string testSubFolderName = new AnalyzeValues().Replace(input: testName, pattern: new AnalyzeValues().TestFolderNamePatterns, keywords: new AnalyzeValues().FolderNameKeywords);
+
+            Assert.AreEqual(ExpectedSubFolderName, testSubFolderName);
+
+            // index of first '_' is right after product number
+            int productNameLength = keywords.IndexOf('_') + 1;
+
+            // split replacement words to insert test file name and new short name for "Regulator #" in the file name, also append file extension.
+            for (int CurrentRegulatorValue = 0; CurrentRegulatorValue < 3; CurrentRegulatorValue++)
+            {
+                string modifiedFolderName = Path.Combine("modified files", testSubFolderName);
+                string regulatorFolderName = $"regulator {CurrentRegulatorValue + 1}";
+                string profileFolderName = $"profile {keywords.Substring(productNameLength + 1, 1)}";
+                string testFolderName = Path.Combine(modifiedFolderName, Path.Combine(regulatorFolderName, new Regex(@"(?<profile>\b[Pp](\d)\b\w*)", RegexOptions.None, TimeSpan.FromMilliseconds(100)).IsMatch(FileNameWithoutRevision) ? profileFolderName : string.Empty)).ToLower();
+                string newFileName = Path.Combine(Path.Combine(Path.GetDirectoryName(FileNameWithPath), testFolderName), $"{keywords.Substring(0, productNameLength)}{testName}_Reg {CurrentRegulatorValue + 1}_{keywords.Substring(productNameLength, keywords.Length - productNameLength)}{Path.GetExtension(FileNameWithPath)}");
+
+                string ExpectedNewFileName = $@"\\eng04-win10\c$\Users\tbircek\Desktop\m6200b\drb#186-run-up\ready-for-modbus-mod-test\{testName.ToLower()}\regulator {CurrentRegulatorValue + 1}\M-6200B_Runup_Reg {CurrentRegulatorValue + 1}_";
+
+                Assert.AreEqual(ExpectedNewFileName, newFileName);
+            }
+
+
+        }
+    }
+}
