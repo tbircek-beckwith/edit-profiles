db0a40a4ceada75886d87df94505979205b85fa0
tbircek <tbircek@beckwithelectric.com>
2020-05-28 07:47:40 -0400

    adds squirrel packaging tool

---
+++ edit-profiles.package/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edit_profiles.package
+{
+    public class Class1
+    {
+    }
+}
